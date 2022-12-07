a120ec82a930610e9212c018dde061084cb26665
shani kupiec <shani.kupiec@gmail.com>
2022-12-07 18:39:16 +0200

    ./

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,16 +1,33 @@
-// import img from '../../public/images/troll-face.png'
+import { useState } from "react";
+import memsData from "../assets/data/memsData";
+import getRandomInt from "../services/helpers";
 
-function Meme() {  
+function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memsData);
+
+  function getRandomMeme() {
+    const randomInt = getRandomInt(0, allMemeImages.data.memes.length);
+    setMeme((prevState) => ({ ...prevState, randomImage: allMemeImages.data.memes[randomInt].url }));
+  }
+
+  // getRandomMeme();
+
   return (
     <main className="main">
-      <form>
-        <input type="text" placeholder="Top Text"/>
-        <input type="text" placeholder="Bottom Text"/>
-        <button>Get a new meme image 🖼️</button>
-        
-      </form>
+      <section className="main__form">
+        <input type="text" placeholder="Top Text" />
+        {/* value={meme.topText} */}
+        <input type="text" placeholder="Bottom Text" />
+        <button onClick={getRandomMeme}>Get a new meme image 🖼️</button>
+      </section>
+      <img src={meme.randomImage} className="main__img" alt="" />
     </main>
-  )
+  );
 }
 
-export default Meme
+export default Meme;
